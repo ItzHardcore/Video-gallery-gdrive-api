45d32fb46a1643afb1f9e9723f48a9e5d42c8549
ItzHardcore <bruno.vp.1999@gmail.com>
2023-01-19 19:00:08 +0000

    almost done

--- video-gallery/src/App.js
+++ video-gallery/src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import MyNavbar from "./components/navbar";
 import Content from "./components/content";
 import Footer from "./components/footer";
+import FloatingMedia from "./components/FloatingMedia";
 
 function App() {
   return (
     <div>
       <MyNavbar />
       <Content />
+      <FloatingMedia />
       <Footer />
     </div>
   );
---
+++ video-gallery/src/components/FloatingMedia.js
@@ -0,0 +1,30 @@
+import "../FloatingApp.css";
+
+function FloatingApp() {
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
+      ></link>
+      <div class="s-soft">
+        <a href="/" class="s-item facebook">
+          <span class="fa fa-facebook"></span>
+        </a>
+        <a href="/" class="s-item twitter">
+          <span class="fa fa-twitter"></span>
+        </a>
+        <a href="/" class="s-item pinterest">
+          <span class="fa fa-pinterest"></span>
+        </a>
+        <a href="/" class="s-item linkedin">
+          <span class="fa fa-linkedin"></span>
+        </a>
+        <a href="/" class="s-item instagram">
+          <span class="fa fa-instagram"></span>
+        </a>
+      </div>
+    </div>
+  );
+}
+export default FloatingApp;
