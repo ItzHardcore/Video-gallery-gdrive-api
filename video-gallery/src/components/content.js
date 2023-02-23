9f92db541b7b706aba780d96d0b3ecff1e8270fa
ItzHardcore <bruno.vp.1999@gmail.com>
2023-02-23 22:34:19 +0000

    update content

--- video-gallery/src/components/content.js
+++ video-gallery/src/components/content.js
@@ -58,7 +58,7 @@ function Content() {
         id="gallery"
         className="h1-responsive font-weight-bold text-center my-4"
       >
-        Video Gallery
+        Galeria de Vídeos
       </h2>
       <FileList></FileList>
 
@@ -79,9 +79,9 @@ function Content() {
         <a
           className="btn-primary btn-lg "
           href="/Curriculo_Margarida_Mimo.pdf"
-          download="CV_Bruno.pdf"
+          download="CV_MargaridaMimo.pdf"
         >
-          <i class="fa fa-download"></i> DOWNLOAD
+          <i class="fa fa-download"></i> DESCARREGAR
         </a>
       </div>
 
@@ -94,16 +94,10 @@ function Content() {
           Contactos
         </h2>
 
-        <p class="text-center w-responsive mx-auto mb-5">
-          Do you have any questions? Please do not hesitate to contact us
-          directly. Our team will come back to you within a matter of hours to
-          help you.
-        </p>
-
         <div class="row">
           <div class="col-md-4 text-center">
             <i class="fas fa-map-marker-alt mt-4 fa-2x"></i>
-            <p>Lisbon, Portugal</p>
+            <p>Lisboa, Portugal</p>
           </div>
           <div class="col-md-4 text-center">
             <i class="fas fa-phone mt-4 fa-2x"></i>
